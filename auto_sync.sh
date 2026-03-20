3eda46876efecd6f1219a9954856ad0e9be5d27b
funkevertz-png <funkevertz-png@users.noreply.github.com>
2026-03-20 15:40:21 +0800

    auto sync: 2026-03-20 15:40:21

--- auto_sync.sh
+++ auto_sync.sh
@@ -3,5 +3,5 @@ cd /c/Users/Administrator/gitck
 git add -A
 if ! git diff --cached --quiet; then
   git commit -m "auto sync: $(date '+%Y-%m-%d %H:%M:%S')"
-  git push origin main
+  git push origin master
 fi
